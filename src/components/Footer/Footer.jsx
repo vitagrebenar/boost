2c52b15dbd70e59ca22ba51c054cb2e7fab9b90f
vitagrebenar <vita.grebenar@gmail.com>
2026-07-28 18:43:16 +0200

    Fix footer icons and animations

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,61 @@
 import React from 'react'
 import './Footer.css'
-import { assets } from '../../assets/assets'
+
+import homeUnactive from '../../assets/home_unactive.png'
+import homeActive from '../../assets/home_active.png'
+import userUnactive from '../../assets/user_unactive.png'
+import userActive from '../../assets/user_active.png'
+import cartUnactive from '../../assets/cart_unactive.png'
+import cartActive from '../../assets/cart_active.png'
+
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
-
-  
-
   return (
     <div className='footer' id='footer'>
       <div className="footer-content">
 
-        <Link to='/'><div className="footer-content-left"></div></Link>
+        <Link to='/' className="footer-link">
+          <img
+            src={homeUnactive}
+            className="footer-icon home-icon"
+            alt="Home"
+          />
+          <img
+            src={homeActive}
+            className="footer-icon home-icon-active"
+            alt="Home"
+          />
+        </Link>
 
-        <Link to='/profile'><div className="footer-content-center"></div></Link>
+        <Link to='/profile' className="footer-link">
+          <img
+            src={userUnactive}
+            className="footer-icon user-icon"
+            alt="Profile"
+          />
+          <img
+            src={userActive}
+            className="footer-icon user-icon-active"
+            alt="Profile"
+          />
+        </Link>
 
-        <Link to='/cart'><div className="footer-content-right"></div></Link>
-        
+        <Link to='/cart' className="footer-link">
+          <img
+            src={cartUnactive}
+            className="footer-icon cart-icon"
+            alt="Cart"
+          />
+          <img
+            src={cartActive}
+            className="footer-icon cart-icon-active"
+            alt="Cart"
+          />
+        </Link>
 
-
-      </div>      
-    </div> 
+      </div>
+    </div>
   )
 }
 
