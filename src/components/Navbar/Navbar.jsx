48d0ae9c819eaa9bcb5e801ecba38f277b29d839
vitagrebenar <vita.grebenar@gmail.com>
2026-07-28 19:01:49 +0200

    Fix navbar and footer icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,26 +3,72 @@ import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 
-const Navbar = ({setShowLogin}) => {
-  return (
+import profilePic from '../../assets/profile_pic.png'
+import profilePicHover from '../../assets/profile_pic_hover.png'
+import searchIcon from '../../assets/search.png'
 
+const Navbar = ({ setShowLogin }) => {
+  return (
     <div>
       <div className='navbar'>
-        <Link to='/'><img className='meni-lijevo' src={assets.novi_logo} alt="" /></Link>
+
+        <Link to='/'>
+          <img 
+            className='meni-lijevo' 
+            src={assets.novi_logo} 
+            alt="" 
+          />
+        </Link>
 
         <ul className='meni-centar'>
-          <li><img className='lokacija' src={assets.location} alt="" /></li>
-          <li><p className='lokacija-tekst'>Croatia, CRO</p></li>
+          <li>
+            <img 
+              className='lokacija' 
+              src={assets.location} 
+              alt="" 
+            />
+          </li>
+
+          <li>
+            <p className='lokacija-tekst'>
+              Croatia, CRO
+            </p>
+          </li>
         </ul>
 
-        <div className='meni-desno' onClick={()=>setShowLogin(true)}></div>
+        <div 
+          className='meni-desno' 
+          onClick={() => setShowLogin(true)}
+        >
+          <img 
+            src={profilePic} 
+            className='profile-icon profile-icon-normal'
+            alt="Profile"
+          />
+
+          <img 
+            src={profilePicHover} 
+            className='profile-icon profile-icon-hover'
+            alt="Profile"
+          />
+        </div>
+
       </div>
 
-      <div class="search">
-        <input type="text" class="searchTerm" placeholder="Search..."></input>
+      <div className="search">
+        <input 
+          type="text" 
+          className="searchTerm" 
+          placeholder="Search..."
+        />
+        <img 
+          src={searchIcon} 
+          className="search-icon"
+          alt="Search"
+        />
       </div>
+
     </div>
-
   )
 }
 
